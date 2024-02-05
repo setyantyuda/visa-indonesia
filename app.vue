9ce5f16c2fdf08c912d2e8e530e7dd25c7257011
Bayu Setyantyuda <setyantyuda@gmail.com>
2024-02-06 03:41:21 +0700

    adjustment

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="bg-primary w-full relative md:flex h-screen overflow-hidden">
     <Sidebar 
       :handleShow="() => handleShow()" 
-      :class="`${mobileNav === true ? 'left-0' : '-left-96' } md:sticky z-50 fixed transition-all duration-500 h-screen bg-primary md:w-[25%] blcok'`"
+      :class="`${mobileNav === true ? 'left-0' : '-left-96' } md:sticky z-50 fixed transition-all duration-500 h-screen bg-primary md:w-[25%] block'`"
     />
     
     <!-- icon for show mobile navbar -->
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="overflow-y-auto bg-white md:rounded-tl-3xl h-full rounded-t-[36px] w-full md:px-10 px-6 md:py-12 pt-10 pb-24">
+    <div class="overflow-y-auto bg-white md:rounded-tl-3xl h-full rounded-t-3xl w-full md:px-10 px-6 md:py-12 pt-10 pb-24">
         <slot />
     </div>
 </template>
